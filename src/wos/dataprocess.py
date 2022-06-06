ca3f06766cbd2b9ace9950b61a6defce66c9b3ec
oceanumeric <numerical.ocean@gmail.com>
2022-06-07 00:51:38 +0200

    [add] new reading post

---
+++ src/wos/dataprocess.py
@@ -0,0 +1,47 @@
+import os
+import xlrd
+import pandas as pd
+
+
+def combine_record_citation():
+    '''
+    A function to combine two excel files:
+        - record.xlsx includes author, title, abstract, etc. 
+        - citation.xlsx includes backward citation 
+    '''
+    innobem = pd.DataFrame()
+    path = '/Users/Michael/Documents/wos/innobem/'
+    for i in range(1, 11):
+        record = 'record'+str(i)+'.xls'
+        citation = 'citation'+str(i)+'.xls'
+        # read file
+        record = path + record 
+        citation = path + citation
+        df1 = pd.read_excel(record)
+        df2 = pd.read_excel(citation)
+        # select column names 
+        columns1 = ['UT (Unique WOS ID)', 'Authors','Author Full Names',
+                    'Article Title', 'Source Title','Author Keywords', 
+                    'Keywords Plus', 'Abstract', 'Addresses', 'Affiliations',
+                    'Publication Year', 'DOI']
+        columns2 = ['Cited References', 'Cited Reference Count']
+        # merge dataset
+        df1 = df1[columns1]
+        df2 = df2[columns2]
+        df_combine = pd.concat([df1, df2], ignore_index=True, axis=1)
+        innobem = pd.concat([innobem, df_combine], ignore_index=True)
+    
+    innobem.columns = ['wos_id', 'authors', 'full_names', 'title', 'journal',
+                       'keywords', 'extra_keywords', 'abstract', 'address',
+                       'affiliation', 'year', 'doi', 'backward_citations', 
+                       'backward_citation_count',]
+    
+    innobem.to_csv('./src/wos/wos_dataset.csv', index=False)
+    
+
+    
+    
+
+if __name__ == "__main__":
+    print(os.getcwd())
+    combine_record_citation()
