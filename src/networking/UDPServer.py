15dc3b0c128adecef6104bb9f9b1e46946557ff1
oceanumeric <numerical.ocean@gmail.com>
2022-06-19 20:18:49 +0200

    [update] server script

--- src/networking/UDPServer.py
+++ src/networking/UDPServer.py
@@ -4,11 +4,12 @@ from socket import *
 server_port = 12000
 server_socket = socket(AF_INET, SOCK_DGRAM)
 
-server_socket.bind(('47.108.238.80', server_port))
+server_socket.bind(('0.0.0.0', server_port))
 
 print("The server is ready to receive")
 
 while True:
     message, client_address = server_socket.recvfrom(2048)
+    print(message.decode())
     modified_message = message.decode().upper()
     server_socket.sendto(modified_message.encode(), client_address)
