2b24d25e27cbc4e9e8f6c18ba216c9bfdfec6809
oceanumeric <numerical.ocean@gmail.com>
2023-04-14 15:36:44 +0200

    [add] new script

---
+++ scripts/br_ch8.R
@@ -0,0 +1,52 @@
+library(pacman)
+p_load(
+    tidyverse, data.table, dtplyr, reshape2,
+    kableExtra, janitor, readxl, png, Cairo, rbenchmark,
+    httr, jsonlite, fedmatch, patchwork,
+    corrplot, tidygraph, ggraph, igraph,
+    treemap, splitstackshape, stringr, lubridate,
+    poweRlaw, voronoiTreemap, ggridges, bayesrules,
+    DescTools, stringi, kit, pheatmap, rstan, bayesplot
+)
+# set option
+options(dplyr.summarise.inform = FALSE)
+options(jupyter.plot_mimetypes = "image/png")
+options(repr.plot.width = 8, repr.plot.height = 5)
+
+gray_scale <- c("#F3F4F8", "#D2D4DA",  "#bcc0ca",
+                "#D3D3D3", "#B3B5BD", "#838383",
+                "#9496A1", "#7d7f89", "#777986",
+                "#656673", "#5B5D6B", "#4d505e",
+                "#404352", "#2b2d3b", "#2B2B2B", "#282A3A",
+                "#1b1c2a", "#191a2b", "#141626", "#101223")
+color_scale <- c("#BB4444", "#EE9988", "#FFFFFF",
+                            "#77AADD", "#4477AA")
+blue_scale <- c("#DBE7F5", "#95BCE3",
+                "#699BCB", "#6597C7", "#4879A9",
+                "#206BAD", "#11549B")
+
+cor_col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF",
+                            "#77AADD", "#4477AA"))
+
+# check working directory
+getwd()
+
+peepsample <- function(dt) {
+    dt %>%
+    .[sample(.N, 5)] %>%
+    kable("pipe")
+}
+
+peephead <- function(dt, x=5) {
+    dt %>%
+    head(x) %>%
+    kable("pipe")
+}
+
+
+## load data
+data("moma_sample")
+
+moma_sample %>% str()
+
+# not very interesting example
